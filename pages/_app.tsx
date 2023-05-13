62a3045c45a84bfe1fd42ba61de75dcd800219ec
nacmonad <macnomad84@gmail.com>
2023-05-12 23:31:38 -0400

    session rpvoider setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
-import { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react"
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return (
-    <Component {...pageProps} />
-  );
-};
-
-export default App;
+export default function App({
+    Component,
+    pageProps: { session, ...pageProps },
+  }) {
+    return (
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    )
+  }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,25 @@
 import React from "react"
-import { GetStaticProps } from "next";
 import Layout from "../components/Layout"
-import Post, { PostProps } from "../components/Post"
+import { PostProps } from "../components/Post"
 
-export const getStaticProps: GetStaticProps = async () => {
-  const feed = []
-  return { 
-    props: { feed }, 
-    revalidate: 10 
-  }
-}
-
+import { useSession } from "next-auth/react";
 type Props = {
   feed: PostProps[]
 }
 
-const Blog: React.FC<Props> = (props) => {
+const Home: React.FC<Props> = (props) => {
+  const {data : session }  = useSession()
+  console.log("sessionData", session);
   return (
     <Layout>
       <div className="page">
-        <h1>Public Feed</h1>
+        <h1>Sago Rewards</h1>
         <main>
-          {props.feed.map((post) => (
-            <div key={post.id} className="post">
-              <Post post={post} />
-            </div>
-          ))}
+          
         </main>
       </div>
     </Layout>
   )
 }
 
-export default Blog
+export default Home
